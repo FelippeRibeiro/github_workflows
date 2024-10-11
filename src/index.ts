62c8908f47753938227fd1f663913ba32e99e164
FelippeRibeiro <felipper43@gmail.com>
2024-10-10 21:15:06 -0300

    WIP: test connect directly in the host and excecute build commands

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,7 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+setInterval(() => {
+  console.log('GitHUB Actions app is running');
+}, 5000);
